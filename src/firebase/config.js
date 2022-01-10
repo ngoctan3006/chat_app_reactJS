5204a37104bc8e50b65e984179b9a68e6b694a2c
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-01-11 04:07:58 +0700

    use firebase to login with facebook

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/analytics'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyA7yY1UvqWT9HCTOBiK6yBP_xoLc6wFR0A',
+    authDomain: 'chat-app-reactjs-6d83b.firebaseapp.com',
+    projectId: 'chat-app-reactjs-6d83b',
+    storageBucket: 'chat-app-reactjs-6d83b.appspot.com',
+    messagingSenderId: '935556442736',
+    appId: '1:935556442736:web:d06a022d7a2082e193a2b5',
+    measurementId: 'G-FTL0WNKHG1'
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
+
+const auth = firebase.auth()
+const db = firebase.firestore()
+
+export { db, auth }
+export default firebase
