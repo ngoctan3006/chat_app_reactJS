815f108c2f7a4f5f16d5cefab8b618000b8ce396
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-01-15 20:50:47 +0700

    add format Date, get data from firebase

--- src/components/ChatRoom/Message.js
+++ src/components/ChatRoom/Message.js
@@ -1,4 +1,5 @@
 import { Avatar, Typography } from 'antd';
+import { formatRelative } from 'date-fns';
 import React from 'react';
 import styled from 'styled-components';
 
@@ -21,17 +22,29 @@ const WrapperStyled = styled.div`
     }
 `;
 
+const formatDate = (seconds) => {
+    let formattedDate = '';
+    if (seconds) {
+        formattedDate = formatRelative(new Date(seconds * 1000), new Date());
+        formattedDate =
+            formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
+    }
+    return formattedDate;
+};
+
 const Message = ({ text, displayName, createdAt, photoURL }) => {
     return (
         <WrapperStyled>
             <div>
                 <Avatar size='small' src={photoURL}>
-                    T
+                    {photoURL ? '' : displayName?.charAt(0).toUpperCase()}
                 </Avatar>
                 <Typography.Text className='author'>
                     {displayName}
                 </Typography.Text>
-                <Typography.Text className='date'>{createdAt}</Typography.Text>
+                <Typography.Text className='date'>
+                    {formatDate(createdAt?.seconds)}
+                </Typography.Text>
             </div>
             <div>
                 <Typography.Text className='content'>{text}</Typography.Text>
