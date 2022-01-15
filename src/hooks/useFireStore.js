2cf0f37aa409b3052cc3df4c7344d15a1c465899
ngoctan3006 <ngoctana1k2014@gmail.com>
2022-01-15 09:58:23 +0700

    get document form firebase

---
+++ src/hooks/useFireStore.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import { db } from '../firebase/config';
+
+const useFireStore = (collection, condition) => {
+    const [documents, setDocuments] = useState([]);
+
+    useEffect(() => {
+        let collectionRef = db.collection(collection).orderBy('createdAt');
+        if (condition) {
+            if (!condition.compareValue) return;
+            collectionRef = collectionRef.where(
+                condition.fieldName,
+                condition.operator,
+                condition.compareValue
+            );
+        }
+
+        const unsubscribe = collectionRef.onSnapshot((snapshot) => {
+            const documents = snapshot.docs.map((doc) => ({
+                ...doc.data(),
+                id: doc.id
+            }));
+
+            setDocuments(documents);
+        });
+
+        return unsubscribe;
+    }, [collection, condition]);
+
+    return documents;
+};
+
+export default useFireStore;
